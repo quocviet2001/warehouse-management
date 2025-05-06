64cd1260cb8970e3c9ec51c94c84d531bd10f336
{quocviet} <{quocviet160101@gmail.com}>
2025-05-06 16:17:54 +0700

    Initial project setup: Spring Boot backend and React frontend1

---
+++ warehouse-backend/src/test/java/com/warehouse/warehouse_backend/WarehouseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warehouse.warehouse_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
